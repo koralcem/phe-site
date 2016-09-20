5ccd7878a61ea460b4f2e5a1bed969a3aac7b60e
Cem Schemel <koralcem@gmail.com>
2016-09-20 15:28:12 +0000

    Create videos in gallery from a ruby hash in controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,5 +12,10 @@ class StaticPagesController < ApplicationController
   end
 
   def media
+    @videos = {
+      'https://www.youtube.com/watch?v=JYbRRRG6Ovg' => 'Test1',
+      'https://www.youtube.com/watch?v=06_Cgscgwus' => 'Van',
+      'https://www.youtube.com/watch?v=EaNzcVglPdo' => 'Psikoloji',
+    }
   end
 end
