8b0f72cd064b2427bae37984c6f6f07d652f257c
Cem Schemel <koralcem@gmail.com>
2018-05-06 11:52:43 -0700

    Shrink the map when the body is narrower than 600px

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-$(document).ready(function() {
-  $(document).click(function(event) {
+$(document).ready(() => {
+  $(document).click((event) => {
     if ($(event.target).hasClass("dropdown")) {
       $(".dropdown-content").toggle("fast")
       event.preventDefault()
     }
     else {
       $(".dropdown-content").hide()
+    }
+  })
+
+  $(window).resize(() => {
+    const maxMapDimension = 600
+    if ($("body").width() < maxMapDimension) {
+      $("#map").width("100%")
+      $("#map").height($("#map").width())
+    }
+    else {
+      $("#map").width(maxMapDimension)
+      $("#map").height(maxMapDimension)
     }
   })
 })
